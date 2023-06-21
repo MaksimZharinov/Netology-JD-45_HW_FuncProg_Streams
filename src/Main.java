a313205c2a014a89659b76b6e82d42bf22d16031
Maksim Zharinov <minimum.cold1991@gmail.com>
2023-06-22 03:44:06 +0700

    creare gitignore & do task1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> doThis = new ArrayList<>(intList);
+        for (Iterator<Integer> it = doThis.iterator(); it.hasNext();) {
+            int tmp = it.next();
+            if (tmp <= 0 || tmp % 2 != 0) {
+                it.remove();
+            }
+        }
+        Collections.sort(doThis);
+        System.out.println(doThis);
     }
 }
