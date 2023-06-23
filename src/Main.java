6edb9ba427288c397ecbc2fdac5d14f00ec5b5fa
Maksim Zharinov <minimum.cold1991@gmail.com>
2023-06-24 02:53:24 +0700

    change Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,19 @@ public class Main {
         }
         Collections.sort(doThis);
         System.out.println(doThis);
+
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+        
     }
 }
