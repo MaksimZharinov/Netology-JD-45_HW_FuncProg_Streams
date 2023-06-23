17ba4bd3fc780700116f0655f2bc7c369bf03552
Maksim Zharinov <minimum.cold1991@gmail.com>
2023-06-24 04:39:53 +0700

    change Main.java & do task2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
         List<Integer> doThis = new ArrayList<>(intList);
-        for (Iterator<Integer> it = doThis.iterator(); it.hasNext();) {
+        for (Iterator<Integer> it = doThis.iterator(); it.hasNext(); ) {
             int tmp = it.next();
             if (tmp <= 0 || tmp % 2 != 0) {
                 it.remove();
@@ -26,6 +27,29 @@ public class Main {
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
         }
-        
+
+        int countYong = (int) persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
+        System.out.println("Количество несовершеннолетних: " + countYong);
+
+        List<String> goArmy = persons.stream()
+                .filter(person -> person.getSex().toString() == "MAN")
+                .filter(person -> person.getAge() >= 18)
+                .filter(person -> person.getAge() <= 27)
+                .map(person -> person.getFamily())
+                .collect(Collectors.toList());
+        System.out.println("Подлежат призыву: " + goArmy);
+
+        List<Person> goWork = persons.stream()
+                .filter(person -> {
+                    if (person.getSex().toString() == "MAN") {
+                        return person.getAge() <= 65 && person.getAge() >= 18;
+                    } else return person.getAge() <= 60 && person.getAge() >= 18;
+                })
+                .filter(person -> person.getEducation().toString() == "HIGHER")
+                .sorted(Comparator.comparing(person -> person.getFamily()))
+                .collect(Collectors.toList());
+        System.out.println("Работоспособное население: " + goWork);
     }
 }
